b857097de459c00337653b0915ec90ed111cd7b7
Alexandre Rinaldi alexandre.rinaldi@essec.edu <alexandre.rinaldi@essec.edu>
2020-04-19 09:48:31 +0200

    hey

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'games/new'
+  get 'games/score'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'ask', to: 'questions#ask'
   get 'answer', to: 'answers#answer'
